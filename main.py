64851ac8aaae61a197d889d4259141722bdb73a8
ccsfarm <u11182s@gmail.com>
2022-02-05 20:12:57 +0300

    1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 import os
 from datetime import datetime, timedelta
 
-directory = "/home/ccsfarm/test"  # "/tank01/backups"
+directory = "/tank01/backups" #target dir
 
 
 class Cleaner:
@@ -13,26 +13,27 @@ class Cleaner:
         self.files = []
         self.current_time = datetime.now()
         self.deadline = self.current_time - timedelta(days=7)  # date - days
+        self.filename = ''
         self.del_count = 0
         self.save_count = 0
 
     def read(self):
         self.files = os.listdir(self.dir)
         self.files = list(filter(lambda x: x.endswith('.tar.gz'), self.files))
-        print(self.files)
 
     def action(self):
-        for filename in self.files:
-            target = filename.rsplit(sep='.')
+        for self.filename in self.files:
+            target = self.filename.rsplit(sep='.')
             target = datetime.strptime(target[2][:-6], "%d-%m-%Y")  # string to date
-            print(filename)
-            if target < self.deadline:
-                print(f"{target},{self.current_time},{self.deadline}, delete")
+            if target < self.deadline and target.day != 20:
+                self.del_target()
                 self.del_count += 1
             else:
-                print(f"{target},{self.current_time},{self.deadline}, save")
                 self.save_count += 1
 
+    def del_target(self):
+        path = os.path.join(os.path.abspath(self.dir), self.filename)
+        os.remove(path)
 
 example = Cleaner(directory)
 example.read()
